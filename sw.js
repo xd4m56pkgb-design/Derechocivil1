52897589a2ff8e7fd8adb9efc455f1ddb201c934
xd4m56pkgb-design <xd4m56pkgb@privaterelay.appleid.com>
2025-11-20 19:46:38 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("civil-test-cache").then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./icon-192.png",
+        "./icon-512.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(resp => {
+      return resp || fetch(e.request);
+    })
+  );
+});
